356f89c4970686708dea0556d95ff6a4480b6982
LTFYR <lutfiyariea@code.edu.az>
2022-07-06 14:41:21 +0400

    Add project files.

---
+++ Vesperr/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using Vesperr.DAL;
+using Vesperr.Models;
+
+namespace Vesperr.Controllers
+{
+    public class HomeController:Controller
+    {
+        private readonly AppDbContext _context;
+
+        public HomeController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            List<Services> list = _context.Services.ToList();
+            return View(list);
+        }
+
+        public IActionResult Detail(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            Services services = _context.Services.FirstOrDefault(x => x.Id == id);
+            return View(services);
+        }
+    }
+}
---
+++ Vesperr/DAL/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Vesperr.Models;
+
+namespace Vesperr.DAL
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options):base(options)
+        {
+
+        }
+
+        public DbSet<Services> Services { get; set; }
+
+    }
+}
